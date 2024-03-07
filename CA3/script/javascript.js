215608fd0a327c532c65da456acb4d7f8fd30150
Fraser McCarter <114738535+spxllz@users.noreply.github.com>
2024-03-07 23:02:30 +0000

    Fixed mobile view + tied up loose ends
    Added external hyperlinks, fixed visit margins, cleaned up css and js, added comments, added map js

--- CA3/script/javascript.js
+++ CA3/script/javascript.js
@@ -1,3 +1,4 @@
+// Scrollwheel JS
 window.addEventListener('scroll', function() {
     var scrollIcon = document.getElementById('scroll-icon');
     if (window.scrollY === 0) {
@@ -5,4 +6,15 @@ window.addEventListener('scroll', function() {
     } else {
         scrollIcon.style.opacity = '0'; // Hide the scroll icon when scrolled down
     }
+});
+
+// Rotate of the dropdown icon JS
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownMenuButton = document.getElementById('dropdownMenuButton');
+    var dropdownIcon = document.getElementById('dropdownIcon');
+
+    dropdownMenuButton.addEventListener('click', function () {
+        // Toggle the rotation of the dropdown icon
+        dropdownIcon.classList.toggle('rotate-icon');
+    });
 });
